5ca39b68a99a071608e0fbb94db978bf4ae009b4
“Nitish-2000” <nitishmani111@gmail.com>
2024-03-26 22:40:29 +0530

    Dart day 2

--- test.dart
+++ test.dart
@@ -5,6 +5,15 @@ void main() {// entry point of dart program / application
  print("5"*8); //Output : printing 5 by 8times    multiplying a string by int gives int times 
 
 //variables:
+int abc = 56 // non decimal values
+double aaa = 56;  // allows decimal
+String abb = "Nitis"; // allows strings
+bool isbbb = true; // allows boolean
+
+dynamic ccc = "dhfsdhgdsf" //dynamic can have any value string or int or bool or double
+var a = "dsfhskj";
+print(a);
+
  
 
 }
