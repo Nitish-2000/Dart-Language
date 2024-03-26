672fded1a47186f03430f89bc2b3650a9242a938
Nitish2000 <nitishmani111@gmail.com>
2024-03-26 13:38:00 +0530

    dart get started

---
+++ test.dart
@@ -0,0 +1,10 @@
+void main() {// entry point of dart program / application
+ print("Hello Talkies"); 
+ print(3+2); //Adding two numbers
+ print("3"+"2"); // Addin two strings is concatenation
+ print("5"*8); //Output : printing 5 by 8times    multiplying a string by int gives int times 
+
+//variables:
+ 
+
+}
